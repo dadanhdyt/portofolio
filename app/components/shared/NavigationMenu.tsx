895e7b935681d46c2b5d07feac245c4d41fd909d
dadanhdyt <dadanhidyt@gmail.com>
2024-01-13 19:20:53 +0700

    Update

--- app/components/shared/NavigationMenu.tsx
+++ app/components/shared/NavigationMenu.tsx
@@ -1,17 +1,34 @@
-import React from "react"
+import React from "react";
 
-export default function NavigationMenu(){
-    const [MenuOpen,setMenuOpen] = React.useState(false);
-    return (
-       <div className="">
-         <button onClick={()=>setMenuOpen(!MenuOpen)} className="border sm:hidden block ring-background-500 rounded ring-2 py-1 px-5">MENU</button>
-         <div className={`${MenuOpen ? 'flex' : 'hidden'} sm:flex`}>
-            <ul className="flex items-center justify-between space-x-6  text-text">
-                <li><a className="hover:text-primary transition-all" href="">About</a></li>
-                <li><a className="hover:text-primary transition-all" href="">Projects</a></li>
-                <li><a className="hover:text-primary transition-all" href="">Contact</a></li>
-            </ul>
-        </div>
-       </div>
-    )
+export default function NavigationMenu() {
+  const [MenuOpen, setMenuOpen] = React.useState(false);
+  return (
+    <div className="">
+      <button
+        onClick={() => setMenuOpen(!MenuOpen)}
+        className="border sm:hidden block ring-background-500 rounded ring-2 py-1 px-5"
+      >
+        MENU
+      </button>
+      <div className={`${MenuOpen ? "flex" : "hidden"} sm:flex`}>
+        <ul className="flex items-center justify-between space-x-6  text-text">
+          <li>
+            <a className="hover:text-primary transition-all" href="">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-primary transition-all" href="">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-primary transition-all" href="">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
